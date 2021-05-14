e71daf2023e5d2489b4bfc8ca38465faa593f34a
Koko Lingga <koko.lingga@gmail.com>
2021-05-14 22:09:27 +0700

    [refactor] proper flow

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 )
 
 /*
-
 Example 1:
 Price of bitcoin each hour
 Hour 1: $5
@@ -13,7 +12,6 @@ Hour 2: $4
 Hour 3: $3
 Hour 4: $2
 Hour 5: $1
-
 In this scenario, the maximum profit Jacky can make is: 0
 
 Example 2:
@@ -24,16 +22,8 @@ Hour 3: $1
 Hour 4: $5
 Hour 5: $6
 Hour 6: $2
-
 In this scenario, the maximum profit Jacky can make is: $5, he will buy at 3rd hour and sell at 5th hour.
-
 */
-
-type profit struct {
-	lowestHour  int
-	lowestPrice int64
-	myDecision  decision
-}
 
 type decision int
 
@@ -42,53 +32,70 @@ const (
 	selling
 )
 
-func get_max_profit(prices []int64) profit {
+type profitTracker struct {
+	lowestBuyingHour    int
+	lowestBuyingPrice   int64
+	highestSellingHour  int
+	highestSellingPrice int64
+	maxProfit           int64
+}
+
+func newProfitTracker() *profitTracker {
+	return &profitTracker{}
+}
+
+func getMaxProfit(prices []int64, pt *profitTracker) int64 {
 	if len(prices) == 0 {
-		return profit{}
+		return 0
 	}
 
-	// lowest price timing
-	// for initial value, let's take the first element of the slice.
-	lowest_buying_price := prices[0]
-	lowest_buying_hour := 0
-	myDecision := buying
-
-	// fmt.Println("Initial value : ")
-	// fmt.Println("first checking Index : ", lowest_buying_hour)
-	fmt.Println("first checking Price : ", lowest_buying_price)
-	fmt.Println("....................")
+	// (initial state)
+	// as an initial step in decision making,
+	// let's use the first element of the slice.
+	pt.lowestBuyingPrice = prices[0]
+	fmt.Println("(initial price checking)")
+	fmt.Printf("%s : %v\n\n", "price", pt.lowestBuyingPrice)
 
 	for index := 1; index < len(prices); index++ {
-		// index start from 1 (skip the first element)
-		// fmt.Println("current Index : ", index)
-		fmt.Println("current Price : ", prices[index])
+		if prices[index] < pt.lowestBuyingPrice {
+			fmt.Printf("%20s : %12v\n", "current lowest price", pt.lowestBuyingPrice)
+			fmt.Printf("%20s : %12v\n", "next hour price", prices[index])
+			fmt.Printf("%s\n", "[it's cheaper. let's buy]")
+			fmt.Printf("%s\n\n", "[marked as the lowest price]")
+			pt.lowestBuyingPrice = prices[index]
+			pt.lowestBuyingHour = index
+		}
 
-		if myDecision == buying {
-			if prices[index] < lowest_buying_price && index > lowest_buying_hour {
-				fmt.Println("ah, it's cheaper. let's buy.")
-				lowest_buying_price = prices[index]
-				lowest_buying_hour = index
-			}
+		if prices[index] > pt.lowestBuyingPrice {
+			fmt.Printf("%20s : %12v\n", "current lowest price", pt.lowestBuyingPrice)
+			fmt.Printf("%20s : %12v\n", "next hour price", prices[index])
+			fmt.Printf("%s\n", "[a raise. should we sell?]")
 
-			if prices[index] > lowest_buying_price {
-				fmt.Printf("[lowest price : %v] a raise. should we sell?\n", lowest_buying_price)
+			profit_amount := prices[index] - pt.lowestBuyingPrice
+			fmt.Printf("%20s : %12v\n\n", "estimated profit", profit_amount)
+
+			if profit_amount > pt.maxProfit {
+				pt.maxProfit = profit_amount
+				pt.highestSellingHour = index
+				pt.highestSellingPrice = prices[index]
 			}
 		}
-		fmt.Println("....................")
-
 	}
 
-	return profit{lowestPrice: lowest_buying_price, lowestHour: lowest_buying_hour, myDecision: myDecision}
+	return pt.maxProfit
 }
 
 func main() {
-	// var prices = []int64{3, 2, 1, 5, 6, 2}
+	var prices = []int64{3, 2, 1, 5, 6, 2}
 	// var prices = []int64{5, 4, 3, 2, 1}
-	var prices = []int64{137147048, 102162326, 199268418, 198975474, 253639272, 356694498, 225661554, 315177788, 328486079, 337443096, 279363057}
+	// var prices = []int64{137147048, 102162326, 199268418, 198975474, 253639272, 356694498, 225661554, 315177788, 328486079, 337443096, 279363057}
 
-	profitData := get_max_profit(prices)
+	pt := newProfitTracker()
 
-	fmt.Println("Lowest Price : ", profitData.lowestPrice)
-	fmt.Println("Lowest Index : ", profitData.lowestHour)
-	fmt.Println("My Decision : ", profitData.myDecision)
+	maxProfit := getMaxProfit(prices, pt)
+
+	fmt.Printf("cheapest Buying : Hour[%v] : %v\n", pt.lowestBuyingHour, pt.lowestBuyingPrice)
+	fmt.Printf("highest Selling : Hour[%v] : %v\n", pt.highestSellingHour, pt.highestSellingPrice)
+	fmt.Printf("maxProfit : %v\n", pt.maxProfit)
+	fmt.Printf("maxProfit : %v\n", maxProfit)
 }
